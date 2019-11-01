2641dfec2f924a6e63cf98701e31dcb47c16d77c
Suyatna <zuyatna@gmail.com>
2019-11-01 17:09:55 +0700

    adding finger swipe

---
+++ Assets/Scripts/Rotations.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Rotations : MonoBehaviour
+{
+
+    private GameObject obj;
+    private int angle;
+    private bool n, e, s, w;
+
+    private void Awake()
+    {
+        obj = this.gameObject;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void DoRotation(int angle)
+    {
+        this.angle = angle;
+        obj.transform.Rotate(0, angle, 0);
+    }
+}
